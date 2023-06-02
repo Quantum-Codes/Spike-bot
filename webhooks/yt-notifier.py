389b601a84141be7424debae7de3ac56ea12c03c
Quantum-Codes <87054411+Quantum-Codes@users.noreply.github.com>
2023-06-02 08:31:04 +0000

    added code

--- webhooks/yt-notifier.py
+++ webhooks/yt-notifier.py
@@ -1,1 +1,8 @@
-print("I ran")
+from discord import Webhook
+import aiohttp, os
+
+async def message():
+  async with aiohttp.ClientSession() as session:
+    webhook = Webhook.from_url(os.environ["webhook_url"], session=session)
+    await webhook.send("heyy")
+    print("sent lol")
