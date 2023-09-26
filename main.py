2e483c9b88f33f49960a73b2ab7cc30197e044d4
Quantum-Codes <87054411+Quantum-Codes@users.noreply.github.com>
2023-09-26 15:10:47 +0000

    Add rolestats and Country tracking

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if "REPL_SLUG" not in os.environ: #detect replit
 
 
 bot = discord.Bot(intents=discord.Intents.all())
-guild_ids = [1099306183426326589] #HARDCODED IN OTHER PLACES
+guild_ids = [1099306183426326589]#HARDCODED IN OTHER PLACES
 
 @bot.event
 async def on_ready():
@@ -33,10 +33,25 @@ async def on_member_join(member):
   if member.guild.id == 1099306183426326589:
     await bot.get_channel(1116003307694067772).send(f'Welcome to the server, {member.mention}! Enjoy your stay here.')
 
+@bot.event
+async def on_member_update(before, after):
+  if before.roles != after.roles:
+    embed = discord.Embed(
+        title= "Country Count",
+        color= discord.Colour.blurple(),
+    )
+    for item in (1149669861132357693,1149670051608277002,1149670359902203925,1149670166263779379,1149669976526032896,1149670113587511387):
+      role = bot.get_guild(1099306183426326589).get_role(item)
+      embed.add_field(name = role.name, value = len(role.members))
+    channel = bot.get_channel(1149749715840274585)
+    message = await channel.fetch_message(1156230990402965616)
+    await message.edit(embed=embed)
+
 bot.load_extension("commands.general")
 bot.load_extension("commands.webhook") #-- add back if needed youtube commands
 bot.load_extension("commands.brawlstars")
 bot.load_extension("commands.menus")
+bot.load_extension("commands.rolestat")
 keep_alive()
 try:
   bot.run(os.environ["token"])
