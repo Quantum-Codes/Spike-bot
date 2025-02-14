d3cf719ffe9f3567eb6d779279a87b10bdd9a13c
Ankit Sinha <87054411+Quantum-Codes@users.noreply.github.com>
2025-02-14 10:06:54 +0530

    Update yt-subscriber.py

--- webhooks/yt-subscriber.py
+++ webhooks/yt-subscriber.py
@@ -4,7 +4,7 @@ x = requests.post(
                  "https://pubsubhubbub.appspot.com/subscribe",
                  data = {
                     "hub.callback": "http://us3.bot-hosting.net:20033/feed",
-                    "hub.topic": "https://www.youtube.com/feeds/videos.xml?channel_id=UCyjy3LTL7AIV_Iwf4A9PeGw",
+                    "hub.topic": "https://www.youtube.com/xml/feeds/videos.xml?channel_id=UCyjy3LTL7AIV_Iwf4A9PeGw",
                     "hub.verify": "sync",
                     "hub.mode": "subscribe",
                     "hub.verify_token": "",
